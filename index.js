6609f7f4f152bed0c0afdcfb848ed96ce33fcc6b
cjpalacios07 <cjpalacios07@gmail.com>
2021-12-19 22:00:14 -0500

    Adding places controller

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-
 const express = require('express')
 const app = express()
 
@@ -10,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-   res.status(404).send('<h1>404 Page</h1>') 
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
