fe14c4ab9e2b308636b46f7541f2c6fc4c231d40
cjpalacios07 <cjpalacios07@gmail.com>
2021-12-22 16:30:14 -0500

    Adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
+const placesList = require('../models/places.js')
 
 router.get('/', (req, res) => {
+    
     res.send('GET /places')
 })
 
+router.get('/:placesId', (req, res) => {
+    const placesId = req.params.placesId
+    res.send('GET /:placesId ${placesId}')
+})
 module.exports = router
