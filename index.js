dfa0297b589796c3e073141697f24fc7d846cb9a
cjpalacios07 <cjpalacios07@gmail.com>
2022-01-02 22:48:06 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-// Express 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
+app.use(express.static('public'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
@@ -19,5 +20,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-// Listen 
+// Listen for Connections
 app.listen(process.env.PORT)
---
+++ views/places/index.jsx
@@ -0,0 +1,33 @@
+const React = require('react')
+const Def = require('../default')
+
+function index (data) {
+  console.log(data)
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div className="col-sm-6">
+        <h2>{place.name}</h2>
+        <p clasName="text-center">
+          {place.cuisines}
+        </p>
+        <img src={place.pic} alt={place.name} width="400" height="400"/>
+        <p clasName="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    )
+  })
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              <div className='row'>
+                {placesFormatted}
+              </div>
+              
+          </main>
+      </Def>
+  )
+  }
+
+module.exports = index
