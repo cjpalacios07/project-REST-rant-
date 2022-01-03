06e48b97a7d94b373ca092b1dff969d5e0b2f818
cjpalacios07 <cjpalacios07@gmail.com>
2022-01-02 23:30:15 -0500

    Adding Styling

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,12 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
+              <div>
+              <img id="image" src="/images/james-barker-v3-zcCWMjgM-unsplash.jpeg" alt="dog"/>
+                <div>
+                Photo by <a href="https://unsplash.com/@barkernotbaker?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">James Barker</a> on <a href="https://unsplash.com/s/photos/dogs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
       </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img id="image" src ="/images/rezel-apacionado-nLz2z-JvCe4-unsplash.jpeg" alt="blueberry oatmeal"/>
+                <div>
+                Photo by <a href="https://unsplash.com/@rezelkealoha?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rezel Apacionado</a> on <a href="https://unsplash.com/photos/MsTOg6rhRVk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>  
               <a href="/places">
               <button className="btn-primary">Places Page</button>
               </a>
