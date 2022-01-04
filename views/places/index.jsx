1af40ed003961f0c17bd7ab78a5d25403eaf9239
cjpalacios07 <cjpalacios07@gmail.com>
2022-01-03 20:09:19 -0500

    Show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,14 +2,18 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-  let placesFormatted = data.places.map((place) => {
+  let placesFormatted = data.places.map((place, index) => {
     return (
-      <div className="col-sm-6">
-        <h2>{place.name}</h2>
+      <div clasName="col-sm-6">
+        <h2>
+          <a href={`/places/${index}`} >
+            {place.name}
+          </a>
+        </h2>
         <p clasName="text-center">
           {place.cuisines}
         </p>
-        <img src={place.pic} alt={place.name} width="400" height="400"/>
+        <img src={place.pic} alt={place.name} />
         <p clasName="text-center">
           Located in {place.city}, {place.state}
         </p>
